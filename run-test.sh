75390066850bd42778e3a950bea1a4f4497f29b1
Jeff Bush <jeffbush001@gmail.com>
2015-09-29 19:40:58 -0700

    Remove clone step, because source tree is now mapped into container

--- run-test.sh
+++ run-test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/jbush001/NyuziProcessor.git root/NyuziProcessor
-cd /root/NyuziProcessor/tests/cosimulation
+cd tests/cosimulation
 ./generate_random.py -m 5
 cd ../..
 make test
